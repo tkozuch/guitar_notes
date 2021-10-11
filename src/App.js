8fd0cc5577da3d9003f010b8ba0c2cab68d19e7a
tkozuch <kozuch.tomasz1@gmail.com>
2021-10-11 10:45:39 +0200

    First versio - headers, and some logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+import notes from "./notes.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCaretDown);
 
 function App() {
+  const [expandedNote, setExpandedNote] = useState(-1);
+  const [inExpandedState, setInExpandedState] = useState(false);
+  const [songNotes, setSongNotes] = useState(notes);
+
+  function toggleNoteExpansion(index) {
+    if (inExpandedState) {
+      setInExpandedState(false);
+    } else {
+      setExpandedNote(index);
+      setInExpandedState(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header">Co grać na gitarze</div>
+        {!inExpandedState
+          ? songNotes.map((note, index) => {
+              return (
+                <div className="note-header" key={index}>
+                  <span className="note-header__text">{note.title}</span>
+                  <button onClick={() => toggleNoteExpansion(index)}>
+                    <FontAwesomeIcon icon="caret-down" size="3x" />
+                  </button>
+                </div>
+              );
+            })
+          : (() => {
+              const note = songNotes[expandedNote];
+              return (
+                <div className="note-header">
+                  <span className="note-header__text">{note.title}</span>
+                  <button onClick={() => toggleNoteExpansion()}>
+                    <FontAwesomeIcon icon="caret-down" size="3x" />
+                  </button>
+                </div>
+              );
+            })()}
+      </div>
     </div>
   );
 }
