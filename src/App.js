3f099be06d7f35685266f8f158bb52a0a3d4fde0
tkozuch <kozuch.tomasz1@gmail.com>
2021-10-30 16:57:36 +0200

    Fix writing backwards and no title after expansion errors

--- src/App.js
+++ src/App.js
@@ -1,58 +1,37 @@
 import "./App.css";
-import notes from "./notes.json";
-import { convertFromRaw, convertToRaw } from "draft-js";
-// import { RichText } from "./components/RichText";
 
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCaretDown,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
+import { ContentState } from "draft-js";
 import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-library.add(faCaretDown);
+library.add(faCaretDown, faMinus, faPlus);
 
-const EMPTY_STATE = {
-  blocks: [],
-  entityMap: {},
+const rawState = {
+  content: {
+    blocks: [
+      {
+        key: "5u4m2",
+        text: " ",
+        type: "unstyled",
+        depth: 0,
+        inlineStyleRanges: [],
+        entityRanges: [],
+        data: {},
+      },
+    ],
+    entityMap: {},
+  },
 };
-
-const content = {
-  entityMap: {},
-  blocks: [
-    {
-      key: "637gr",
-      text: "Initialized from content state.",
-      type: "unstyled",
-      depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [],
-      data: {},
-    },
-  ],
-};
-
-// function App() {
-//   const [state, setState] = useState(null);
-
-//   useEffect(() => {
-//     console.log("state, ", state);
-//   }, [state]);
-
-//   return (
-//     <>
-//       <Editor
-//         defaultContentState={content}
-//         onContentStateChange={(content) => {
-//           setState(content);
-//         }}
-//       ></Editor>
-//       <Editor contentState={state}></Editor>
-//     </>
-//   );
-// }
+console.log("RS created.", rawState);
 
 function App() {
   const [expandedNote, setExpandedNote] = useState({
@@ -62,42 +41,74 @@ function App() {
       content: undefined,
     },
   });
-  const [songNotes, setSongNotes] = useState(notes);
+  const [songNotes, setSongNotes] = useState([]);
 
   const inExpandedState = () => expandedNote.index !== -1;
 
   function toggleNoteExpansion(index) {
     if (inExpandedState()) {
-      setExpandedNote({ index: -1, note: undefined });
+      setExpandedNote({
+        index: -1,
+        note: { title: undefined, content: undefined },
+      });
     } else {
       setExpandedNote({ index, note: songNotes[index] });
     }
   }
 
-  useEffect(() => {
+  function deleteNote(index) {
+    const notesCopy = [...songNotes];
+    notesCopy.splice(index, 1);
+    setSongNotes(notesCopy);
+  }
+
+  const saveData = useCallback(
+    function () {
+      console.log("Saving data: ", songNotes);
+      fetch("http://127.0.0.1:8000/notes", {
+        method: "POST",
+        body: JSON.stringify(songNotes),
+      });
+    },
+    [songNotes]
+  );
+
+  useEffect(function initializeData() {
     fetch("http://127.0.0.1:8000/notes", {
       method: "GET",
     })
       .then((response) => response.json())
       .then((notesData) => {
-        console.log("goot", notesData);
+        console.log("Data initialized: ", notesData);
         setSongNotes(notesData);
       });
   }, []);
 
+  useEffect(
+    function setEventListeners() {
+      let checkEventPressed = function (event) {
+        if (event.ctrlKey && event.key === "s") {
+          event.preventDefault();
+          saveData();
+        }
+      };
+      document.addEventListener("keydown", checkEventPressed);
+      return () => {
+        document.removeEventListener("keydown", checkEventPressed);
+      };
+    },
+    [saveData]
+  );
+
   useEffect(() => {
     if (inExpandedState()) {
       const songNotesCopy = [...songNotes];
-      console.log("n: ", songNotesCopy[expandedNote.index], expandedNote.index);
       songNotesCopy[expandedNote.index].content = expandedNote.note.content;
       setSongNotes(songNotesCopy);
-      console.log("About to POST");
-      fetch("http://127.0.0.1:8000/notes", {
-        method: "POST",
-        body: JSON.stringify(songNotes),
-      });
     }
   }, [expandedNote.note.content]);
+
+  console.log("CS", ContentState());
 
   return (
     <div className="App">
@@ -105,16 +116,40 @@ function App() {
         <div className="header">Co grać na gitarze</div>
 
         {!inExpandedState() ? (
-          songNotes.map((note, index) => {
-            return (
-              <div className="note-header" key={index}>
-                <span className="note-header__text">{note.title}</span>
-                <button onClick={() => toggleNoteExpansion(index)}>
-                  <FontAwesomeIcon icon="caret-down" size="3x" />
-                </button>
-              </div>
-            );
-          })
+          [
+            ...songNotes.map((note, index) => {
+              return (
+                <div className="note-header" key={index}>
+                  <span className="note-header__text">{note.title}</span>
+                  <button
+                    className="note-header__minus-button"
+                    onClick={() => deleteNote(index)}
+                    key={index}
+                  >
+                    <FontAwesomeIcon icon="minus" size="2x"></FontAwesomeIcon>
+                  </button>
+                  <button
+                    className="note-header__toggle-button"
+                    onClick={() => toggleNoteExpansion(index)}
+                  >
+                    <FontAwesomeIcon icon="caret-down" size="3x" />
+                  </button>
+                </div>
+              );
+            }),
+            <button
+              onClick={function addSong() {
+                const name = prompt("Song name:");
+                setSongNotes([
+                  ...songNotes,
+                  { title: name, content: undefined },
+                ]);
+              }}
+            >
+              <FontAwesomeIcon icon="plus" size="3x"></FontAwesomeIcon>
+              Add
+            </button>,
+          ]
         ) : (
           <>
             <div className="note-header">
@@ -129,15 +164,21 @@ function App() {
               <Editor
                 toolbarClassName="rich-text__toolbar"
                 onContentStateChange={(contentState) => {
+                  const n = {
+                    ...expandedNote.note,
+                    content: contentState,
+                  };
+                  console.log("setting: ", n);
                   setExpandedNote({
                     index: expandedNote.index,
-                    note: {
-                      ...expandedNote,
-                      content: contentState,
-                    },
+                    note: n,
                   });
                 }}
-                contentState={expandedNote.note.content}
+                initialContentState={
+                  expandedNote.note.content
+                    ? expandedNote.note.content
+                    : rawState.content
+                }
               ></Editor>
             </div>
           </>
